39432d19496c93be7457791eae0c3ec246d59df3
hopeswanke <hopeswanke@gmail.com>
2020-10-08 13:51:06 -0400

    10/7 work

---
+++ ttt.js
@@ -0,0 +1,70 @@
+let button = document.getElementById('start');
+
+button.addEventListener('click', () => {
+    alert('yellow');
+    button.disabled = true;
+})
+
+// ----------------------------- Cell EventListeners ---------------------------------
+let cell1 = document.getElementById('cell1')
+
+cell1.addEventListener('click', () => {
+    cell1.style.backgroundColor = "pink";
+    cell1.disabled = true;
+})
+
+let cell2 = document.getElementById('cell2')
+
+cell2.addEventListener('click', () => {
+    cell2.style.backgroundColor = "pink";
+    cell2.disabled = true;
+})
+
+let cell3 = document.getElementById('cell3')
+
+cell3.addEventListener('click', () => {
+    cell3.style.backgroundColor = "pink";
+    cell3.disabled = true;
+})
+
+let cell4 = document.getElementById('cell4')
+
+cell4.addEventListener('click', () => {
+    cell4.style.backgroundColor = "pink";
+    cell4.disabled = true;
+})
+
+let cell5 = document.getElementById('cell5')
+
+cell5.addEventListener('click', () => {
+    cell5.style.backgroundColor = "pink";
+    cell5.disabled = true;
+})
+
+let cell6 = document.getElementById('cell6')
+
+cell6.addEventListener('click', () => {
+    cell6.style.backgroundColor = "pink";
+    cell6.disabled = true;
+})
+
+let cell7 = document.getElementById('cell7')
+
+cell7.addEventListener('click', () => {
+    cell7.style.backgroundColor = "pink";
+    cell7.disabled = true;
+})
+
+let cell8 = document.getElementById('cell8')
+
+cell8.addEventListener('click', () => {
+    cell8.style.backgroundColor = "pink";
+    cell8.disabled = true;
+})
+
+let cell9 = document.getElementById('cell9')
+
+cell9.addEventListener('click', () => {
+    cell9.style.backgroundColor = "pink";
+    cell9.disabled = true;
+})
