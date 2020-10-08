b95b28c76ef881d1190d4b10b0e60e36faa8517d
Dfaye023 <70286830+Dfaye023@users.noreply.github.com>
2020-10-08 16:46:24 -0400

    changes

--- ttt.js
+++ ttt.js
@@ -1,19 +1,38 @@
-// ----------------------------- Button -----------------------------
+// ----------------------------------- Button -------------------------------------
 let button = document.getElementById('start');
 
 button.addEventListener('click', () => {
     alert('Let the Game Begin');
     button.disabled = true;
 })
-// ----------------------------- Cell EventListeners -------------------------------
+// ------------------------------ Cell Status -------------------------------------
 let cell = document.querySelectorAll('.cell')
+let error = document.getElementById('error-message')
 for (let item of cell) {
-    item.addEventListener('click', () => {
-        item.style.backgroundColor = "pink";
-        item.disabled = true;
-    }) 
+    item.addEventListener('click', function cellFiller() {
+        if (player === "X") {
+            item.textContent = "X";
+            item.style.fontSize = "250px"
+            item.style.justifyContent = 'center';
+            item.removeEventListener('click', cellFiller)
+        }
+        else if (player === "O") {
+            item.textContent = "O";
+            item.style.fontSize = "250px";
+            item.style.justifyContent = 'center';
+            item.removeEventListener('click', cellFiller)
+        }
+        changeStatus()
+    })
 }
-// ------------------------------- Player Status -----------------------------------
-let player = "X"
+
+// ------------------------------ Player Status -----------------------------------
+let player = 'X'
 let computer = "O"
-  
+let statusDisplay = document.getElementById('player-turn')
+
+function changeStatus() {
+    player = player === "X" ? "O" : "X";
+    statusDisplay.innerHTML = "Player Turn: " + player
+}
+// ---------------------------------
